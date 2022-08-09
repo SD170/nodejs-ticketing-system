6c3089f9c26a84608cabc13a6d3ebf3d23590257
SD170 <duttasaswata7@gmail.com>
2022-08-09 07:39:16 +0530

    express setup

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("object");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const fs_1 = __importDefault(require("fs"));
+//load env vars
+dotenv_1.default.config({ path: __dirname + '/../config.env' });
+// console.log(__dirname+'/../config.env');
+// console.log(process.env.MONGOPORT);
+const app = (0, express_1.default)();
+//body parser
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, morgan_1.default)('common', {
+    stream: fs_1.default.createWriteStream('./morgan.log', { flags: 'a' })
+}));
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') { //only when using dev env
+    app.use((0, morgan_1.default)('dev'));
+}
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+//handle unhandled PromeseRejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close Server & exit process
+    server.close(() => process.exit(1));
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,44 @@
-console.log("object");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import fs from "fs";
+
+//load env vars
+dotenv.config({path: __dirname+'/../config.env'});
+
+// console.log(__dirname+'/../config.env');
+// console.log(process.env.MONGOPORT);
+
+
+const app = express();
+
+
+//body parser
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(morgan('common', {
+    stream: fs.createWriteStream('./morgan.log', {flags: 'a'})
+}));
+//dev logging middleware
+if(process.env.NODE_ENV==='development'){   //only when using dev env
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+});
+
+//handle unhandled PromeseRejection
+process.on('unhandledRejection',(err:any,promise)=>{
+    console.log(`Error: ${err.message}`);
+    
+    //Close Server & exit process
+    server.close(()=> process.exit(1));
+})
+
